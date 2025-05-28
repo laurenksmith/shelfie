d74658b0e1e2d45e43f995140949a60cd7a660d1
Lauren Smith <laurenkimsmith@gmail.com>
2025-05-28 10:02:58 +0100

    Creating a function which will allow the user to view all their books on their shelf(ie!)

---
+++ view_books.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+# A function to view all books
+def view_all_books(connection):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM books;")
+        rows = cursor.fetchall()
+
+        if rows:
+            print("\nAll Books in Shelfie:")
+            for row in rows:
+                print(row)
+
+        else:
+            print("\nNo Books found.")
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
